94808efbaed3f361cabdb7f0c479ef45df90d1d0
chintanbhargave <chintanbhargave@gmail.com>
2020-05-07 11:34:11 +0530

    with Feature group and Marker
    Signed-off-by: chintanbhargave <chintanbhargave@gmail.com>

--- WebMap/webmap.py
+++ WebMap/webmap.py
@@ -2,4 +2,8 @@ import folium
 
 map = folium.Map(location = [38.58,-99.09], zoom_start=5, tiles="stamen terrain")
 
+f = folium.FeatureGroup("Map1")
+f.add_child(folium.Marker(location = [38.58,-99.09], popup="MyMarker", icon=folium.Icon(color="red")))
+
+map.add_child(f)
 map.save("BasicMap.html")
