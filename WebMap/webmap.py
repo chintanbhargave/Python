0989bd52f4f41c6fabef828f0e5e5b8cf18a44d5
chintanbhargave <chintanbhargave@gmail.com>
2020-05-07 12:08:00 +0530

    Adding elevation to popup

--- WebMap/webmap.py
+++ WebMap/webmap.py
@@ -5,13 +5,14 @@ vol = pandas.read_csv("USAvolcanoes.txt")
 lat = list(vol["LAT"])
 lon = list(vol["LON"])
 name = list(vol["NAME"])
+elev = list(vol["ELEV"])
 
 map = folium.Map(location = [38.58,-99.09], zoom_start=5, tiles="stamen terrain")
 
 f = folium.FeatureGroup("Map1")
 
-for lt,ln,nm in zip(lat,lon,name):
-    f.add_child(folium.Marker(location = [lt,ln], popup=nm, icon=folium.Icon(color="red")))
+for lt,ln,nm,el in zip(lat,lon,name,elev):
+    f.add_child(folium.Marker(location = [lt,ln], popup=str(el)+"\n"+nm, icon=folium.Icon(color="red")))
 
 
 map.add_child(f)
