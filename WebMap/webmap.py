7a65881804a0e286c412255b7b544009493efbe7
chintanbhargave <chintanbhargave@gmail.com>
2020-05-07 11:55:10 +0530

    Volcano data and adding markers to each volcano location

--- WebMap/webmap.py
+++ WebMap/webmap.py
@@ -1,9 +1,18 @@
 import folium
+import pandas
+
+vol = pandas.read_csv("USAvolcanoes.txt")
+lat = list(vol["LAT"])
+lon = list(vol["LON"])
+name = list(vol["NAME"])
 
 map = folium.Map(location = [38.58,-99.09], zoom_start=5, tiles="stamen terrain")
 
 f = folium.FeatureGroup("Map1")
-f.add_child(folium.Marker(location = [38.58,-99.09], popup="MyMarker", icon=folium.Icon(color="red")))
+
+for lt,ln,nm in zip(lat,lon,name):
+    f.add_child(folium.Marker(location = [lt,ln], popup=nm, icon=folium.Icon(color="red")))
+
 
 map.add_child(f)
 map.save("BasicMap.html")
