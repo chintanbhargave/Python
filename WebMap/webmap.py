8691aae40b8fa3c66bcd38b9e33bfa26b6c91e99
chintanbhargave <chintanbhargave@gmail.com>
2020-05-07 14:45:43 +0530

    Control Layer added (final)

--- WebMap/webmap.py
+++ WebMap/webmap.py
@@ -18,15 +18,18 @@ def color(elevation):
 
 map = folium.Map(location = [38.58,-99.09], zoom_start=5, tiles="stamen terrain")
 
-f = folium.FeatureGroup("Map1")
+fg = folium.FeatureGroup("Volcanoes")
 
 for lt,ln,nm,el in zip(lat,lon,name,elev):
-    f.add_child(folium.CircleMarker(location = [lt,ln], radius=8, popup=str(el)+"\n"+nm, fill_color = color(el), color="grey", fill_opacity=0.7))
+    fg.add_child(folium.CircleMarker(location = [lt,ln], radius=8, popup=str(el)+"\n"+nm, fill_color = color(el), color="grey", fill_opacity=0.7))
 
+fp = folium.FeatureGroup("PopulationPolygons")
 #  To add polygons GeoJson is used. polygon coordinates are in the worldpop.json
-f.add_child(folium.GeoJson(data=open('worldpop.json', 'r', encoding='utf-8-sig').read(),
+fp.add_child(folium.GeoJson(data=open('worldpop.json', 'r', encoding='utf-8-sig').read(),
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(f)
+map.add_child(fg)
+map.add_child(fp)
+map.add_child(folium.LayerControl())
 map.save("BasicMap.html")
