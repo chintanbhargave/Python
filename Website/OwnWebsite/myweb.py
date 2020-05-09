b8df1b868cf0f6b278b51d4aa7c0af77de111c40
chintanbhargave <chintanbhargave@gmail.com>
2020-05-09 15:34:08 +0530

    adding layout css

--- Website/OwnWebsite/myweb.py
+++ Website/OwnWebsite/myweb.py
@@ -1,17 +1,14 @@
 from flask import Flask,render_template
 
-#  Instantiating
 app = Flask(__name__)
-
-#  decorator. return content of the fn will be mapped to the url specified in the decorator
 
 @app.route('/')
 def fn():
     return render_template("home.html")
 
-@app.route('/content/')
+@app.route('/about/')
 def con():
-    return render_template("content.html")
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)    
