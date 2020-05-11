dd5266ca7b39894f02f4a7e0d19091e18e2825b3
chintanbhargave <chintanbhargave@gmail.com>
2020-05-11 15:21:56 +0530

    clear button addded

--- TkinterUI/uiprog.py
+++ TkinterUI/uiprog.py
@@ -11,11 +11,21 @@ def kg():
     t2.insert(END,pounds)
     t3.insert(END,ounces)
 
+
+def clear():
+    t1.delete("1.0","end")
+    t2.delete("1.0","end")
+    t3.delete("1.0","end")
+    e1.delete(first=0,last="end")
+
 b1 = Button(window,text = "convert",command=kg)
-b1.grid(row=0,column=2)
+b1.grid(row=0,column=3)
+
+b2 = Button(window,text = "clear",command=clear)
+b2.grid(row=0,column=0)
 
 l = Label(window,text="kg")
-l.grid(row=0,column=0)
+l.grid(row=0,column=2)
 
 e1_value = StringVar()
 e1 = Entry(window,textvariable=e1_value)
