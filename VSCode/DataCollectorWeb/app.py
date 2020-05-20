097925d01ee95ae776d9a4f6d2a6736632126e50
chintanbhargave <chintanbhargave@gmail.com>
2020-05-20 14:54:58 +0530

    .

--- VSCode/DataCollectorWeb/app.py
+++ VSCode/DataCollectorWeb/app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template, request
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres543@localhost/HeightCollector'
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    __tablename__ = "data"
+    id = db.Column(db.Integer,primary_key=True)
+    email_ = db.Column(db.String(120), unique=True)
+    height_ = db.Column(db.Integer)
+
+    def __init__(self,email_,height_):
+        self.email = email_
+        self.height = height_
+            
 
 @app.route("/")
 def index():
